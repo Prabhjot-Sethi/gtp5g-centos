c04c74a974a0e6e02dd6562904d968b3972edead
Prabhjot Singh Sethi <prabhjot.sethi@gmail.com>
2021-02-03 20:20:59 +0530

    Update install-kernel.sh

--- install-kernel.sh
+++ install-kernel.sh
@@ -3,7 +3,7 @@
 sudo yum -y install https://www.elrepo.org/elrepo-release-7.0-5.el7.elrepo.noarch.rpm
 sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 sudo yum --disablerepo="*" --enablerepo="elrepo-kernel" list available | grep kernel-lt
-sudo yum --enablerepo=elrepo-kernel install kernel-lt
+sudo yum --enablerepo=elrepo-kernel install -y kernel-lt
 
 sudo yum -y --enablerepo=elrepo-kernel install kernel-lt-{devel,headers,perf}
 sudo grub2-set-default 0
